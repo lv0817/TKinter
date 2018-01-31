fcf8d276194b4129fa93069cea3a64fe1bdf2b28
lv0817 <412435484@qq.com>
2018-01-31 09:51:56 +0800

    练习

--- tk12.py
+++ tk12.py
@@ -13,7 +13,12 @@ for i in range(100):
     lb.insert(tk.END,i)
 
 lb.pack(side='left',fill=tk.BOTH)
-sb.config(command=lb.yview)#注意要在其他创建好再去配置某个选项，不然容易出错
+sb.config(command=lb.yview)
+#yview是内部设置y方向如何显示，不用我们关心他是如何实现的 
+#注意要在其他lb创建好再去配置某个选项，不然lb还没有产生就去设置回出错
+'''
+修改一个属性，可以使用.config实现
+'''
 
 
 tk.mainloop()
